64b11bb2590d92641cbb3da5a1e6139317dc9a76
mikky-j <moregunwa@gmail.com>
2023-02-25 13:10:43 +0100

    added delete feature

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const clock = document.getElementById("clock");
 
 setInterval(() => {
-    const date = new Date();
-    clock.textContent = date.toLocaleTimeString();
+  const date = new Date();
+  clock.textContent = date.toLocaleTimeString();
 }, 1000);
 
 const alarms_list = document.getElementById("alarm-container");
@@ -10,30 +10,34 @@ const add_button = document.getElementById("add-alarm");
 
 let alarmTimes = [];
 
-add_button.addEventListener("click", function() {
-    addAlarm();
-})
+add_button.addEventListener("click", function () {
+  addAlarm();
+});
 
 function addAlarm() {
-    const alarm_time = document.getElementById("alarm-time").value;
-    if (alarm_time) {
-        alarmTimes.push(alarm_time);
-        updateAlarms();
-    }
+  const alarm_time = document.getElementById("alarm-time").value;
+  if (alarm_time) {
+    alarmTimes.push(alarm_time);
+    updateAlarms();
+  }
+}
+
+function deleteAlarm(index) {
+  alarmTimes.splice(index, 1);
+  updateAlarms();
 }
 
 function updateAlarms() {
-    alarms_list.innerHTML = "";
-    alarmTimes.forEach((time), (index) => {
-        var alarm = `<div id = "alarm">
+  alarms_list.innerHTML = "";
+  alarmTimes.forEach((time, index) => {
+    var alarm = `<div id = "alarm">
             <p class = "set-time">${time}</p>
             <label class="switch">
                 <input type = "checkbox">
                 <span class="slider round"></span>
             </label>
-            <button id = "del" on><img class = "del" src = "assets/imgs/trash.png" width="20px" height="20px"></button>
+            <button id = "del" onClick=deleteAlarm(${index})><img class = "del" src = "assets/imgs/trash.png" width="20px" height="20px"></button>
         </div>`;
-        alarms_list.innerHTML += alarm;
-        }
-    )
-  }
+    alarms_list.innerHTML += alarm;
+  });
+}
